d9b68f814bd35325dc45402ad72cdc281b73d0cf
Huijie Wei <huijiewei@outlook.com>
2025-03-04 08:36:32 +0800

    build: update dependencies

--- eslint.config.js
+++ eslint.config.js
@@ -2,4 +2,5 @@ import eslintBrowser from "@resolid/config/eslint.browser";
 import eslintNode from "@resolid/config/eslint.node";
 import eslintTypescript from "@resolid/config/eslint.typescript";
 
+/** @type {import("eslint").Linter.Config[]} */
 export default [...eslintTypescript, ...eslintNode, ...eslintBrowser];
