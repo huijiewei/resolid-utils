c023b20d18f5312f07aa7a2416bd0bbb25f0f1e4
Huijie Wei <huijiewei@outlook.com>
2025-10-07 09:19:15 +0800

    feat: update node support version

---
+++ .changeset/tough-onions-stop.md
@@ -0,0 +1,5 @@
+---
+"@resolid/utils": major
+---
+
+update node support version
--- src/object/index.ts
+++ src/object/index.ts
@@ -1,9 +1,9 @@
 /**
- * 从对象中省略指定的键，返回一个新的对象，不影响原始对象。
+ * 创建一个省略指定键的新对象。
  */
 export const omit = <T extends object, K extends keyof T>(
   object: T,
-  keys: K[],
+  keys: readonly K[],
 ): Omit<T, K> => {
   return keys.reduce(
     (acc, key) => {
@@ -12,4 +12,62 @@ export const omit = <T extends object, K extends keyof T>(
     },
     { ...object },
   );
+};
+
+/**
+ * 创建一个新对象，该对象由不满足条件的属性组成。
+ */
+export const omitBy = <T extends object>(
+  object: T,
+  shouldOmit: (value: T[keyof T], key: keyof T) => boolean,
+): Partial<T> => {
+  const result: Partial<T> = {};
+
+  for (const key of Object.keys(object) as Array<keyof T>) {
+    const value = object[key];
+
+    if (!shouldOmit(value, key)) {
+      result[key] = value;
+    }
+  }
+
+  return result;
+};
+
+/**
+ * 创建一个由指定属性组成的新对象。
+ */
+export const pick = <T extends object, K extends keyof T>(
+  object: T,
+  keys: readonly K[],
+): Pick<T, K> => {
+  const result = {} as Pick<T, K>;
+
+  for (const key of keys) {
+    if (Object.hasOwn(object, key)) {
+      result[key] = object[key];
+    }
+  }
+
+  return result;
+};
+
+/**
+ * 创建一个新对象，该对象由满足条件的属性组成。
+ */
+export const pickBy = <T extends object>(
+  object: T,
+  shouldPick: (value: T[keyof T], key: keyof T) => boolean,
+): Partial<T> => {
+  const result: Partial<T> = {};
+
+  for (const key of Object.keys(object) as Array<keyof T>) {
+    const value = object[key];
+
+    if (shouldPick(value, key)) {
+      result[key] = value;
+    }
+  }
+
+  return result;
 };
